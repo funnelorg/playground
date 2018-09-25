e74d0a1b46ddd1d1559e77851b64235c5c2d0349
Ramesh VK <ramesh.vyaghrapuri@gmail.com>
2018-09-25 17:55:27 +0530

    Update to latest funnel

--- eval.go
+++ eval.go
@@ -7,19 +7,26 @@ package main
 
 import (
 	"github.com/funnelorg/funnel"
+	"github.com/funnelorg/funnel/builtin"
 	"github.com/funnelorg/funnel/math"
-	"github.com/funnelorg/funnel/url"	
+	"github.com/funnelorg/funnel/url"
 	"github.com/gopherjs/gopherjs/js"
 )
 
 func Eval(code string, done func(interface{})) {
 	go func() {
-		result := funnel.Eval(url.New(math.New()), "browser", code)
-		if err, ok := result.(error); ok {
+		result := funnel.Eval(url.Scope(math.Scope(builtin.Scope)), "browser", code)
+		if s, ok := result.(stackable); ok {
+			result = s.Stack()
+		} else if err, ok := result.(error); ok {
 			result = map[string]interface{}{"Error": err.Error()}
 		}
 		done(result)
 	}()
+}
+
+type stackable interface {
+	Stack() string
 }
 
 func main() {
